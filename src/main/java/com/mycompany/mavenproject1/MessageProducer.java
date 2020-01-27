789045c3cdb34e573e66d32a9e6fa529310b157f
root <root@ubuntu>
2020-01-27 13:49:15 -0800

    Initial commit. TODO: Message processing. UI Updating.

---
+++ src/main/java/com/mycompany/mavenproject1/MessageProducer.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+/**
+ *
+ * @author root
+ */
+public class MessageProducer implements Runnable {
+    
+    private ConcurrentLinkedQueue<Message> queue;
+    private boolean isRunning = false;
+    private DatagramSocket datagramSocket;
+    
+    public MessageProducer(ConcurrentLinkedQueue<Message> queue) {
+        this.queue = queue;
+    }
+
+    @Override
+    public void run() {     
+        isRunning = true;
+        
+        try {
+            datagramSocket = new DatagramSocket(14550);
+            
+            byte[] receive = new byte[512];
+            
+            while(isRunning) {
+                DatagramPacket datagramPacket = new DatagramPacket(receive, receive.length);
+                
+                try {
+                    datagramSocket.receive(datagramPacket);
+                    
+                    if(datagramPacket.getLength() > 0) {
+                        queue.add(new Message(datagramPacket));
+                    }
+                } catch(IOException ex) {
+                    ex.printStackTrace();
+                } 
+            }
+        } catch (SocketException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    public void shutdown() {
+        isRunning = false;
+        
+        if(datagramSocket != null) {
+            datagramSocket.close();
+        }
+    }
+}
