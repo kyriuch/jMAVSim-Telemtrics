1b9707956765ecab26d6c8d2e59f457632818f4f
kyriuch <>
2020-01-27 13:58:10 -0800

    Renaming main package name.

---
+++ src/main/java/com/mycompany/jmavsimtelemetrics/MainApp.java
@@ -0,0 +1,53 @@
+package com.mycompany.jmavsimtelemetrics;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.util.Scanner;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class MainApp extends Application {
+    
+    private ConcurrentLinkedQueue<Message> queue = new ConcurrentLinkedQueue<>();
+    
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Scene.fxml"));
+        Parent root = loader.load();
+        
+        FXMLController fXMLController = loader.getController();
+        
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("/styles/Styles.css");
+        
+        stage.setTitle("JavaFX and Maven");
+        stage.setScene(scene);
+        stage.show();
+        
+        MessageConsumer messageConsumer = new MessageConsumer(queue);
+        messageConsumer.addMessageReceiver(fXMLController);
+        
+        new Thread(new MessageProducer(queue)).start();
+        new Thread(messageConsumer).start();
+    }
+
+    /**
+     * The main() method is ignored in correctly deployed JavaFX application.
+     * main() serves only as fallback in case the application can not be
+     * launched through deployment artifacts, e.g., in IDEs with limited FX
+     * support. NetBeans ignores main().
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
---
+++ src/main/java/com/mycompany/jmavsimtelemetrics/MessageConsumer.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.jmavsimtelemetrics;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+/**
+ *
+ * @author root
+ */
+public class MessageConsumer implements Runnable {
+
+    private ConcurrentLinkedQueue<Message> queue;
+    private boolean isRunning = false;
+    private List<IMessageReceiver> messageReceivers = new ArrayList<>();
+    
+    public MessageConsumer(ConcurrentLinkedQueue<Message> queue) {
+        this.queue = queue;
+    }
+    
+    @Override
+    public void run() {
+        isRunning = true;
+        
+        while(isRunning) {
+            if(!queue.isEmpty()) {
+                Message message = queue.poll();
+                
+                if(!messageReceivers.isEmpty()) {
+                    for(int i = 0; i < messageReceivers.size(); i++) {
+                        messageReceivers.get(i).ReceiveMessage(message);
+                    }
+                }
+            }
+        }
+    }
+    
+    public void shutdown() {
+        isRunning = false;
+    }
+    
+    public void addMessageReceiver(IMessageReceiver messageReceiver) {
+        messageReceivers.add(messageReceiver);
+    }
+}
